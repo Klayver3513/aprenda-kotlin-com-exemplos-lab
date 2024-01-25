afa38998b240b562ce5ed7bedf229e039f9310ab
Klayver <143143591+Klayver3513@users.noreply.github.com>
2024-01-25 15:26:35 -0300

    desafio dio

--- desafio.kt
+++ desafio.kt
@@ -1,21 +1,48 @@
-// [Template no Kotlin Playground](https://pl.kotl.in/WcteahpyN)
-
 enum class Nivel { BASICO, INTERMEDIARIO, DIFICIL }
 
-class Usuario
+data class Usuario(val name:String)
 
-data class ConteudoEducacional(var nome: String, val duracao: Int = 60)
+data class ConteudoEducacional(val nome: String, val duracao: Int = 60)
 
-data class Formacao(val nome: String, var conteudos: List<ConteudoEducacional>) {
+data class Formacao(val nome: String, val nivel: Nivel, val conteudos: List<ConteudoEducacional>) {
 
     val inscritos = mutableListOf<Usuario>()
     
-    fun matricular(usuario: Usuario) {
-        TODO("Utilize o parâmetro $usuario para simular uma matrícula (usar a lista de $inscritos).")
+    fun matricular(usuario:Usuario){
+		this.inscritos.add(usuario)
     }
 }
 
 fun main() {
-    TODO("Analise as classes modeladas para este domínio de aplicação e pense em formas de evoluí-las.")
-    TODO("Simule alguns cenários de teste. Para isso, crie alguns objetos usando as classes em questão.")
+    var id: Int = 1;
+	 val formacoes: MutableList<Formacao> = mutableListOf(
+        Formacao("basica", Nivel.BASICO, mutableListOf(
+            ConteudoEducacional("Kotlin aula inicial"),
+
+            )),
+        Formacao("media", Nivel.INTERMEDIARIO, mutableListOf(
+            ConteudoEducacional("Kotlin aula intermediário"),
+
+            )),
+        Formacao("Avançada", Nivel.DIFICIL, mutableListOf(
+            ConteudoEducacional("Kotlin aula avançado"),
+ 
+     		)),
+         )
+ 			println(formacoes)
+                for (formacao in formacoes) {
+        when(id) {
+            0 -> formacao.matricular(Usuario("amanda"))
+            1 -> formacao.matricular(Usuario("klayver"))
+            2 -> formacao.matricular(Usuario("davi"))
+        }
+        print("\tNome: ${formacao.nome}\tNivel: ${formacao.nivel}\tInscritos: ")
+        for (usuario in formacao.inscritos) {
+            print("${usuario.name} ")
+        }
+        print("\n\t\tConteúdos:\n")
+        for (conteudo in formacao.conteudos) {
+            println("\t\t\tNome: ${conteudo.nome}  Duração: ${conteudo.duracao} minutos")
+        }
+}
 }
